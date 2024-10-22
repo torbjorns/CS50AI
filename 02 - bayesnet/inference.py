036bdf5ba60c14ddeeda22b8fe528e2d8663f243
Torbjørn Sitre <torbjorn.sitre@gmail.com>
2024-10-22 18:22:19 +0200

    Added the Bayesnet files

--- 02 - bayesnet/inference.py
+++ 02 - bayesnet/inference.py
@@ -1,15 +1,35 @@
+import torch
 from model import model
 
+# This is the observed data, -1 means missing data 
+X = torch.tensor(
+    [
+        [
+            -1,
+            -1,
+            1, # train is delayed
+            -1,
+        ]
+    ]
+)
+
+X_masked = torch.masked.MaskedTensor(X, mask=(X != -1))
+
+states = (
+    ("rain", ["none", "light", "heavy"]),
+    ("maintenance", ["yes", "no"]),
+    ("train", ["on time", "delayed"]),
+    ("appointment", ["attend", "miss"]),
+)
+
 # Calculate predictions
-predictions = model.predict_proba({
-    "train": "delayed"
-})
+predictions = model.predict_proba(X_masked)
 
 # Print predictions for each node
-for node, prediction in zip(model.states, predictions):
+for (node_name, values), prediction in zip(states, predictions):
     if isinstance(prediction, str):
-        print(f"{node.name}: {prediction}")
+        print(f"{node_name}: {prediction}")
     else:
-        print(f"{node.name}")
-        for value, probability in prediction.parameters[0].items():
+        print(f"{node_name}")
+        for value, probability in zip(values, prediction[0]):
             print(f"    {value}: {probability:.4f}")
--- 02 - bayesnet/likelihood.py
+++ 02 - bayesnet/likelihood.py
@@ -1,6 +1,26 @@
+import numpy
+import torch
 from model import model
 
-# Calculate probability for a given observation
-probability = model.probability([["none", "no", "on time", "attend"]])
+
+rain_values = ["none", "light", "heavy"]
+maintenance_values = ["yes", "no"]
+train_values = ["on time", "delayed"]
+appoinment_values = ["attend", "miss"]
+
+
+# Calculate likelihood of this specific combination of events
+probability = model.probability( 
+    torch.as_tensor(
+        [
+            [
+                rain_values.index("none"),
+                maintenance_values.index("no"),
+                train_values.index("on time"),
+                appoinment_values.index("miss"),
+            ]
+        ]
+    )
+)
 
 print(probability)
